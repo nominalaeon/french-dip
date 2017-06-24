702b4792d54a0e330229dfc08897cf2f04c199b2
Noah Rodenbeek <noah@drawnbymouse.com>
2017-06-24 15:17:39 -0500

    Adding shim for Object.assign and a guard clause for when dataset comes back as undefined instead of an empty object …thanks IE

---
+++ gulpfile.babel.js
@@ -0,0 +1,50 @@
+/*!
+ * Endless Aisle Gulpfile
+ * @author VML
+ */
+
+'use strict';
+
+/** globals */
+import gulp from 'gulp';
+import os   from 'os';
+import pkg  from './package.json';
+
+/** plug-ins */
+import addSrc       from 'gulp-add-src';
+import babel        from 'gulp-babel';
+import clean        from 'gulp-clean';
+import concat       from 'gulp-concat';
+import jshint       from 'gulp-jshint';
+import plumber      from 'gulp-plumber';
+import sourcemaps   from 'gulp-sourcemaps';
+import uglify       from 'gulp-uglify';
+
+/** dependencies */
+import pump   from 'pump';
+import rename from 'gulp-rename';
+
+gulp.task('clean', () => {
+  return gulp.src([
+      'french-dip.min.js'
+    ], {
+      read: false
+    })
+    .pipe(clean());
+});
+
+gulp.task('uglify', (cb) => {
+  pump([
+    gulp.src('french-dip.js'),
+    uglify({
+      outSourceMap: true
+    }),
+    rename('french-dip.min.js'),
+    gulp.dest('.')
+  ], cb);
+});
+
+gulp.task('default', [
+  'clean',
+  'uglify'
+]);
